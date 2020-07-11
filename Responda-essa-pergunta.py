78c6351a5cd5ce18db9089aa277e68b8fe5cdc0c
leo-885 <65372884+leo-885@users.noreply.github.com>
2020-07-11 16:11:11 -0300

    Add files via upload

---
+++ Responda-essa-pergunta.py
@@ -0,0 +1,11 @@
+#testando algumas possibilidades
+
+num = float(input("quanto é 0 x 998? "))
+if num <= 0:
+    if num == 0:
+        print("        Acertou!")
+    else:
+        print("        mais")
+else:
+    print("        menos")
+    
